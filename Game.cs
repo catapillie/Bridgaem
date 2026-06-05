2d826249dcec11f9a7b92abcc03cd9d5df4c6ada
catapillie <espritox@gmail.com>
2026-06-05 23:37:31 +0200

    blank window

---
+++ Game.cs
@@ -0,0 +1,33 @@
+using Foster.Framework;
+
+namespace Bridgaem;
+
+public class Game : App
+{
+    public Game() : base(new AppConfig()
+    {
+        ApplicationName = "Bridgaem",
+        WindowTitle = "WESH BRIDGE",
+        Width = 1920,
+        Height = 1080,
+    })
+    {
+
+    }
+
+    protected override void Startup()
+    {
+    }
+
+    protected override void Shutdown()
+    {
+    }
+
+    protected override void Update()
+    {
+    }
+
+    protected override void Render()
+    {
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Bridgaem;
+
+using var game = new Game();
+game.Run();
